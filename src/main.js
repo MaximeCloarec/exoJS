0eb65b47a5c10761158a65b4baec24698b932354
MaximeCloarec <maxime.cloarec@yahoo.fr>
2025-09-03 10:56:37 +0200

    Mise a jour files

---
+++ src/main.js
@@ -0,0 +1,37 @@
+console.log("Hello World from main.js");
+
+const siteName = "Mon super site";
+document.getElementById("title").innerText = siteName;
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("copyright").innerText = `Copyright © ${currentYear} - Tous droits réservés par ${siteName}`;
+
+
+//* On est dans /src/main.js
+
+// ! ⬇ Ancienne version de base (counter)
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
